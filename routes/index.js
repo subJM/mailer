2c22f3f46a1fd7523b36fb99bb0d396235971a45
SJM <thswhdals87@gmail.com>
2024-10-04 16:57:40 +0900

    서버 정보 수정

--- routes/index.js
+++ routes/index.js
@@ -10,27 +10,6 @@ router.get('/', function(req, res, next) {
 
 
 
-router.get('/mails', async function(req, res, next) {
-
-  const emailContent =
-    `안녕하세요, 테스트님.
-
-  고객님의 요청으로, 주문 번호 111의 주문이 성공적으로 삭제되었습니다.
-
-  저희 'HOLO'의 서비스를 이용해주셔서 감사합니다.`;
-
-  await sendEmail({
-      to: 'thswhdals@gmail.com',
-      subject: '이메일 발송 테스트',
-      text: emailContent,
-  });
-
-  res.status(201).send({ result: 'success' });
-
-});
-
-
-
 
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -10,20 +10,13 @@ router.get('/', function(req, res, next) {
 
 
 
-router.get('/mails', async function(req, res, next) {
+router.post('/mails', async function(req, res, next) {
   const admin_email = req.body.admin_email;
   const pass = req.body.pass;
   const receiveEmail = req.body.to;
 
   const emailTitle = req.body.title;
   const emailContent = req.body.content;
-  // const emailContent =
-  //   `안녕하세요, 테스트님.
-
-  // 고객님의 요청으로, 주문 번호 111의 주문이 성공적으로 삭제되었습니다.
-
-  // 저희 'HOLO'의 서비스를 이용해주셔서 감사합니다.`;
-
   
   await sendEmail({
       admin_email: admin_email, 
@@ -32,9 +25,8 @@ router.get('/mails', async function(req, res, next) {
       subject: emailTitle,
       text: emailContent,
   });
-
-  // res.status(201).send({ result: 'success' });
-  res.send('success');
+  res.status(201).send({ result: 'success' });
+  // res.send('success');
 });
 
 module.exports = router;
