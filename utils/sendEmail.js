6942d4b883829e787289950c2ebb0f3aae6bd4fb
SJM <thswhdals87@gmail.com>
2024-10-11 04:10:05 +0900

    보안사항

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -6,6 +6,7 @@ async function sendEmail({admin_email, password , senderName, to, subject, html 
     const transporter = nodemailer.createTransport({
         host: 'smtp.gmail.com', // 사용할 이메일 서비스의 호스트 주소 (gamil)
         port: 587, // 이메일 서비스의 포트 번호 (일반적으로 25, 587, 465, 2525 중 하나 사용)
+        secure: false,
         auth: { // 이메일 서버 인증을 위한 사용자의 이메일 주소와 비밀번호
             user: admin_email, // 이메일 주소
             pass: password, // 이메일 비밀번호 (그대로 노출되기 때문에 구글의 app 패스워드를 사용할 것을 추천한다.)
