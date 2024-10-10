6036083969d8aad191fb18bc405233adca42a4cb
SJM <thswhdals87@gmail.com>
2024-10-10 21:07:58 +0900

    보내는사람 이름 설정

--- routes/users.js
+++ routes/users.js
@@ -14,6 +14,7 @@ router.post('/mails', async function(req, res, next) {
   const admin_email = req.body.admin_email;
   const pass = req.body.pass;
   const receiveEmail = req.body.to;
+  const senderName = req.body.senderName;
 
   const emailTitle = req.body.title;
   const emailContent = req.body.content; // HTML 형식의 콘텐츠
@@ -22,6 +23,7 @@ router.post('/mails', async function(req, res, next) {
     await sendEmail({
       admin_email: admin_email, 
       password: pass,
+      senderName: senderName,
       to: receiveEmail,
       subject: emailTitle,
       html: emailContent, // HTML 형식의 콘텐츠를 사용
--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,20 +1,20 @@
 const nodemailer = require('nodemailer'); // 이메일 전송을 위한 nodemailer 모듈 불러오기
 
 
-async function sendEmail({admin_email, password , to, subject, html }) {
+async function sendEmail({admin_email, password , senderName, to, subject, html }) {
     // 이메일 전송을 위한 메일 서버 연결
     const transporter = nodemailer.createTransport({
         host: 'smtp.gmail.com', // 사용할 이메일 서비스의 호스트 주소 (gamil)
         port: 587, // 이메일 서비스의 포트 번호 (일반적으로 25, 587, 465, 2525 중 하나 사용)
         auth: { // 이메일 서버 인증을 위한 사용자의 이메일 주소와 비밀번호
-            user: 'thswhdals87@gmail.com', // 이메일 주소
-            pass: 'qvxu qdrz oact qwno', // 이메일 비밀번호 (그대로 노출되기 때문에 구글의 app 패스워드를 사용할 것을 추천한다.)
+            user: admin_email, // 이메일 주소
+            pass: password, // 이메일 비밀번호 (그대로 노출되기 때문에 구글의 app 패스워드를 사용할 것을 추천한다.)
         },
     });
 
     // 메일 옵션 설정
     let mailOptions = {
-        from: admin_email,
+        from: senderName,
         to: to,
         subject: subject,
         html: html, // HTML 형식의 콘텐츠를 설정
