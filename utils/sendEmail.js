4d95d6ad220d16fbdc2a1c0ec8f8e7561644264c
SJM <thswhdals87@gmail.com>
2024-10-10 14:23:20 +0900

    fixed untracked files

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -7,8 +7,8 @@ async function sendEmail({admin_email, password , to, subject, text }) {
         host: 'smtp.gmail.com', // 사용할 이메일 서비스의 호스트 주소 (gamil)
         port: 587, // 이메일 서비스의 포트 번호 (일반적으로 25, 587, 465, 2525 중 하나 사용)
         auth: { // 이메일 서버 인증을 위한 사용자의 이메일 주소와 비밀번호
-            user: admin_email, // 이메일 주소
-            pass: password, // 이메일 비밀번호 (그대로 노출되기 때문에 구글의 app 패스워드를 사용할 것을 추천한다.)
+            user: 'thswhdals87@gmail.com', // 이메일 주소
+            pass: 'qvxu qdrz oact qwno', // 이메일 비밀번호 (그대로 노출되기 때문에 구글의 app 패스워드를 사용할 것을 추천한다.)
         },
     });
 
